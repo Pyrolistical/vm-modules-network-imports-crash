f0babcf6cacf55c71e392e16dd804904a9675ad0
Pyrolistical <pyrogx1133@gmail.com>
2022-06-01 13:21:39 -0700

    crashes when vm module is used together with network imports

---
+++ direct-network-imports.js
@@ -0,0 +1,7 @@
+import capitalize from "https://cdn.skypack.dev/pin/lodash-es@v4.17.21-rDGl8YjBUjcrrAbjNrmo/mode=imports/unoptimized/capitalize.js";
+
+console.log(
+  capitalize(
+    "this string should be capitalized using an https import of lodash.capitalize"
+  )
+);
---
+++ vm-module-network-imports.js
@@ -0,0 +1,15 @@
+import vm from "node:vm";
+
+const module = new vm.SourceTextModule(`
+import capitalize from "https://cdn.skypack.dev/pin/lodash-es@v4.17.21-rDGl8YjBUjcrrAbjNrmo/mode=imports/unoptimized/capitalize.js";
+
+console.log(
+  capitalize(
+    "this string should be capitalized using an https import of lodash.capitalize"
+  )
+);
+`);
+
+await module.link(() => {});
+
+await module.evaluate();
